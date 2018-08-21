dc5b299e15000e5ab95a6a353854519311cc3c5f
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2018-08-21 21:47:09 +0100

    refactor and multi tests

--- Dag.fs
+++ Dag.fs
@@ -1,15 +1,15 @@
-namespace global
+namespace DagSystem
 
 open System
 open System.Threading.Tasks
 
-type Dag = {
+type Dag = private {
     InputKeys : obj array
     InputValues : obj array
-    CalculationKeys : obj array
-    CalculationInputs : (Set<int> * Set<int>) array
-    CalculationFunctions : (Dag -> Task<obj>) array
-    CalculationValues : Lazy<Task<obj>> array
+    FunctionKeys : obj array
+    FunctionInputs : (Set<int> * Set<int>) array
+    FunctionFunctions : (Dag -> Task<obj>) array
+    FunctionValues : Lazy<Task<obj>> array
 }
 
 module Dag =
@@ -19,124 +19,121 @@ module Dag =
         a.[Array.length a - 1] <- v
         a
 
-    let inline private taskMap (f:'a->'b) (t:Task<'a>) =
-        t.ContinueWith(fun (r:Task<'a>) -> f r.Result)
+    let inline private taskMap f (t:Task<_>) =
+        t.ContinueWith(fun (r:Task<_>) -> f r.Result)
 
-    type Input = CellInput
-    type Calculation = CellCalculation
+    type Input = private | CellInput
+    type Function = private | CellFunction
 
-    type Cell<'a,'b> = Cell of obj
+    type Cell<'a,'b> = private | Cell of obj
 
     let empty = {
         InputKeys = [||]
         InputValues = [||]
-        CalculationKeys = [||]
-        CalculationInputs = [||]
-        CalculationFunctions = [||]
-        CalculationValues = [||]
+        FunctionKeys = [||]
+        FunctionInputs = [||]
+        FunctionFunctions = [||]
+        FunctionValues = [||]
     }
 
     let addInput (v:'a) (d:Dag) : Dag * Cell<'a, Input> =
         let key = obj()
         { d with
             InputKeys = append d.InputKeys key
-            InputValues = append d.InputValues (box v)
+            InputValues = box v |> append d.InputValues
         }, Cell key
 
-    let getInput (Cell key:Cell<'a,Input>) (d:Dag) : 'a =
+    let getValue (Cell key:Cell<'a,Input>) (d:Dag) : 'a =
         let i = Array.findIndex ((=)key) d.InputKeys
-        d.InputValues.[i] :?> 'a
+        downcast d.InputValues.[i]
 
     let setInput (Cell key:Cell<'a,Input>) (a:'a) (d:Dag) : Dag =
         let i = Array.findIndex ((=)key) d.InputKeys
-        if d.InputValues.[i] :?> 'a = a then d
+        if downcast d.InputValues.[i] = a then d
         else
             let dirtyCalcs =
                 Seq.fold (fun (j,s) (inputs,calcInputs) ->
-                    if Set.contains i inputs || Set.intersect s calcInputs |> Set.isEmpty |> not then
-                        (j+1),Set.add j s
+                    if Set.contains i inputs ||
+                       Set.intersect s calcInputs |> Set.isEmpty |> not then
+                        j+1, Set.add j s
                     else
-                        (j+1),s
-                ) (0,Set.empty) d.CalculationInputs
+                        j+1, s
+                ) (0,Set.empty) d.FunctionInputs
                 |> snd
 
-            let vs = Array.copy d.InputValues
-            vs.[i] <- box a
+            let inputValues = Array.copy d.InputValues
+            inputValues.[i] <- box a
 
-            if Set.isEmpty dirtyCalcs then
-                { d with
-                    InputValues = vs
-                }
+            if Set.isEmpty dirtyCalcs then { d with InputValues = inputValues }
             else
-                let calcValues = Array.copy d.CalculationValues
+                let functionValues = Array.copy d.FunctionValues
                 let dag = {
                     d with
-                        InputValues = vs
-                        CalculationValues = calcValues
+                        InputValues = inputValues
+                        FunctionValues = functionValues
                 }
                 Set.iter (fun i ->
-                    let calc = d.CalculationFunctions.[i]
-                    calcValues.[i] <- lazy calc dag
+                    functionValues.[i] <- lazy d.FunctionFunctions.[i] dag
                 ) dirtyCalcs
                 dag
 
-    let getAsync (Cell key:Cell<'a,Calculation>) (d:Dag) : Task<'a> =
-        let i = Array.findIndex ((=)key) d.CalculationKeys
-        d.CalculationValues.[i].Value |> taskMap (fun o -> downcast o)
+    let getValueTask (Cell key:Cell<'a,Function>) (d:Dag) : Task<'a> =
+        let i = Array.findIndex ((=)key) d.FunctionKeys
+        d.FunctionValues.[i].Value |> taskMap (fun o -> downcast o)
 
     let changed (Cell key:Cell<'a,'t>) (before:Dag) (after:Dag) : bool =
-        if typeof<'t> = typeof<Calculation> then
-            let i = Array.findIndex ((=)key) before.CalculationKeys
-            LanguagePrimitives.PhysicalEquality before.CalculationValues.[i] after.CalculationValues.[i] |> not
+        if typeof<'t> = typeof<Function> then
+            let i = Array.findIndex ((=)key) before.FunctionKeys
+            before.FunctionValues.[i] <> after.FunctionValues.[i]
         else
             let i = Array.findIndex ((=)key) before.InputKeys
-            (before.InputValues.[i] :?> 'a) <> (after.InputValues.[i] :?> 'a)
+            downcast before.InputValues.[i] <> downcast after.InputValues.[i]
 
-    type 'a Builder = {
+    type 'a Builder = private {
         Dag : Dag
         Inputs : Set<int> * Set<int>
         Function : Dag -> Task<'a>
     }
 
-    let build (d:Dag) f = {
+    let buildFunction (d:Dag) f = {
         Dag = d
         Inputs = Set.empty, Set.empty
         Function = fun _ -> Task.FromResult f
     }
 
-    let apply (Cell key:Cell<'a,'t>) (b:Builder<'a->'b>) =
-        let isCalcInput = typeof<'t> = typeof<Calculation>
+    let applyCell (Cell key:Cell<'a,'t>) {Dag=dag;Inputs=inI,inC;Function=bFn} =
+        let isFunctionCell = typeof<'t> = typeof<Function>
         let i =
-            if isCalcInput then b.Dag.CalculationKeys else b.Dag.InputKeys
+            if isFunctionCell then dag.FunctionKeys else dag.InputKeys
             |> Array.findIndex ((=)key)
         {
-            Dag = b.Dag
+            Dag = dag
             Inputs =
-                match isCalcInput, b.Inputs with
-                | true, (iInputs,cInputs) -> iInputs, Set.add i cInputs
-                | false, (iInputs, cInputs) -> Set.add i iInputs, cInputs
+                if isFunctionCell then inI, Set.add i inC
+                              else Set.add i inI, inC
             Function =
-                if isCalcInput then
+                if isFunctionCell then
                     fun d ->
-                        let fTask = b.Function d
-                        ( d.CalculationValues.[i].Value
-                          |> taskMap (fun o -> fTask |> taskMap (fun f -> downcast o |> f))
+                        let fTask = bFn d
+                        ( d.FunctionValues.[i].Value |> taskMap (fun o ->
+                            taskMap (fun f -> downcast o |> f) fTask  )
                         ).Unwrap()
                 else
                     fun d ->
-                        let fTask = b.Function d
-                        fTask |> taskMap (fun f -> downcast d.InputValues.[i] |> f)
+                        bFn d |> taskMap (fun f ->
+                            downcast d.InputValues.[i] |> f  )
         }
 
-    let addCalculation (b:Builder<'a>) : Dag * Cell<'b,Calculation> =
+    let addFunction ({Dag=dag;Inputs=ips;Function=fn}:'a Builder) =
         let key = obj()
-        let calc d = taskMap box (b.Function d)
-        let dag = {
-            b.Dag with
-                CalculationKeys = append b.Dag.CalculationKeys key
-                CalculationInputs = append b.Dag.CalculationInputs b.Inputs
-                CalculationFunctions = append b.Dag.CalculationFunctions calc
-                CalculationValues = append b.Dag.CalculationValues null
+        let calc = fn >> taskMap box
+        let d = {
+            dag with
+                FunctionKeys = append dag.FunctionKeys key
+                FunctionInputs = append dag.FunctionInputs ips
+                FunctionFunctions = append dag.FunctionFunctions calc
+                FunctionValues = append dag.FunctionValues null
         }
-        dag.CalculationValues.[dag.CalculationValues.Length-1] <- lazy calc dag
-        dag, Cell key
+        d.FunctionValues.[d.FunctionValues.Length-1] <- lazy calc d
+        let cell : Cell<'a,Function> = Cell key
+        d, cell
--- DagTests.fs
+++ DagTests.fs
@@ -1,44 +1,47 @@
+open DagSystem
 open Expecto
 
 let tests =
     testList "dag tests" [
         testAsync "one cell" {
             let dag, cell1 = Dag.addInput 7 Dag.empty
-            Expect.equal 7 (Dag.getInput cell1 dag) "one cell"
+            Expect.equal 7 (Dag.getValue cell1 dag) "one cell"
         }
         testAsync "two cell" {
             let dag, cell1 = Dag.addInput 8 Dag.empty
             let dag, cell2 = Dag.addInput 9 dag
-            Expect.equal 8 (Dag.getInput cell1 dag) "first 8"
-            Expect.equal 9 (Dag.getInput cell2 dag) "second 9"
+            Expect.equal 8 (Dag.getValue cell1 dag) "first 8"
+            Expect.equal 9 (Dag.getValue cell2 dag) "second 9"
         }
-        testAsync "one calc" {
+        testAsync "one function" {
             let dag, cell1 = Dag.addInput 42 Dag.empty
             let dag, cell2 =
-                Dag.build dag (fun x -> x * 10)
-                |> Dag.apply cell1
-                |> Dag.addCalculation
-            Expect.equal 420 (Dag.getAsync cell2 dag).Result "42 * 10 = 420"
+                Dag.buildFunction dag (fun x -> x * 10)
+                |> Dag.applyCell cell1
+                |> Dag.addFunction
+            let! result = Dag.getValueTask cell2 dag |> Async.AwaitTask
+            Expect.equal 420 result "42 * 10 = 420"
         }
-
-        testAsync "one calc with set" {
-            let dag, cell1 = Dag.addInput 42 Dag.empty
+        testAsync "one function with set" {
+            let dag, cell1 = Dag.addInput 13 Dag.empty
             let dag, cell2 =
-                Dag.build dag (fun x -> x * 10)
-                |> Dag.apply cell1
-                |> Dag.addCalculation
+                Dag.buildFunction dag (fun x -> x * 10)
+                |> Dag.applyCell cell1
+                |> Dag.addFunction
             let dag = Dag.setInput cell1 43 dag
-            Expect.equal 430 (Dag.getAsync cell2 dag).Result "43 * 10 = 430"
+            let! result = Dag.getValueTask cell2 dag |> Async.AwaitTask
+            Expect.equal 430 result "43 * 10 = 430"
         }
-        testAsync "one calc with set twice" {
-            let dag, cell1 = Dag.addInput 42 Dag.empty
+        testAsync "one function with set twice" {
+            let dag, cell1 = Dag.addInput 15 Dag.empty
             let dag, cell2 =
-                Dag.build dag (fun x -> x * 10)
-                |> Dag.apply cell1
-                |> Dag.addCalculation
+                Dag.buildFunction dag (fun x -> x * 10)
+                |> Dag.applyCell cell1
+                |> Dag.addFunction
             let dag = Dag.setInput cell1 43 dag
             let dag = Dag.setInput cell1 44 dag
-            Expect.equal 440 (Dag.getAsync cell2 dag).Result "44 * 10 = 440"
+            let! result = Dag.getValueTask cell2 dag |> Async.AwaitTask
+            Expect.equal 440 result "44 * 10 = 440"
         }
         testAsync "not changed input" {
             let dagBefore, cell1 = Dag.addInput 42 Dag.empty
@@ -48,44 +51,110 @@ let tests =
         testAsync "changed input" {
             let dagBefore, cell1 = Dag.addInput 42 Dag.empty
             let dagAfter = Dag.setInput cell1 45 dagBefore
-            Expect.isTrue (Dag.changed cell1 dagBefore dagAfter) "input changed"
+            Expect.isTrue (Dag.changed cell1 dagBefore dagAfter) "changed"
         }
-        testAsync "not changed calc" {
+        testAsync "not changed function" {
             let dag, cell1 = Dag.addInput 42 Dag.empty
             let dagBefore, cell2 =
-                Dag.build dag (fun x -> x * 10)
-                |> Dag.apply cell1
-                |> Dag.addCalculation
+                Dag.buildFunction dag (fun x -> x * 10)
+                |> Dag.applyCell cell1
+                |> Dag.addFunction
             let dagAfter,_ = Dag.addInput 45 dagBefore
             Expect.isFalse (Dag.changed cell2 dagBefore dagAfter) "no change"
         }
-        testAsync "changed calc" {
-            let dag, cell1 = Dag.addInput 42 Dag.empty
+        testAsync "changed function" {
+            let dag, cell1 = Dag.addInput 17 Dag.empty
             let dagBefore, cell2 =
-                Dag.build dag (fun x -> x * 10)
-                |> Dag.apply cell1
-                |> Dag.addCalculation
+                Dag.buildFunction dag (fun x -> x * 10)
+                |> Dag.applyCell cell1
+                |> Dag.addFunction
             let dagAfter = Dag.setInput cell1 45 dagBefore
-            Expect.isTrue (Dag.changed cell2 dagBefore dagAfter) "calc changed"
-            Expect.equal 450 (Dag.getAsync cell2 dagAfter).Result "45 * 10 = 450"
-            Expect.equal 420 (Dag.getAsync cell2 dagBefore).Result "42 * 10 = 420"
+            Expect.isTrue (Dag.changed cell2 dagBefore dagAfter) "changed"
+            let! result = Dag.getValueTask cell2 dagAfter |> Async.AwaitTask
+            Expect.equal 450 result "45 * 10 = 450"
+            let! result = Dag.getValueTask cell2 dagBefore |> Async.AwaitTask
+            Expect.equal 170 result "17 * 10 = 170"
         }
-        testAsync "two calc" {
-            let dag, cell1 = Dag.addInput 42 Dag.empty
+        testAsync "chained functions" {
+            let dag, cell1 = Dag.addInput 18 Dag.empty
             let dag, cell2 =
-                Dag.build dag (fun x -> x * 10)
-                |> Dag.apply cell1
-                |> Dag.addCalculation
+                Dag.buildFunction dag (fun x -> x * 10)
+                |> Dag.applyCell cell1
+                |> Dag.addFunction
             let dagBefore, cell3 =
-                Dag.build dag (fun x -> x + 1)
-                |> Dag.apply cell2
-                |> Dag.addCalculation
-            let dagAfter = Dag.setInput cell1 43 dagBefore
-            Expect.equal 431 (Dag.getAsync cell3 dagAfter).Result "431"
-            Expect.equal 421 (Dag.getAsync cell3 dagBefore).Result "421"
+                Dag.buildFunction dag (fun x -> x + 1)
+                |> Dag.applyCell cell2
+                |> Dag.addFunction
+            let dagAfter = Dag.setInput cell1 23 dagBefore
+            let! result = Dag.getValueTask cell3 dagAfter |> Async.AwaitTask
+            Expect.equal 231 result "231"
+            let! result = Dag.getValueTask cell3 dagBefore |> Async.AwaitTask
+            Expect.equal 181 result "181"
+        }
+        testAsync "two function" {
+            let dag, cell1 = Dag.addInput "z" Dag.empty
+            let dag, cell2 = Dag.addInput 7 dag
+            let dag, cell3 =
+                Dag.buildFunction dag (fun s (n:int) -> s + string n)
+                |> Dag.applyCell cell1
+                |> Dag.applyCell cell2
+                |> Dag.addFunction
+            let! result = Dag.getValueTask cell3 dag |> Async.AwaitTask
+            Expect.equal "z7" result "z7"
+        }
+        testAsync "three function with set" {
+            let dag, cell1 = Dag.addInput "f" Dag.empty
+            let dag, cell2 = Dag.addInput 8 dag
+            let dag, cell3 = Dag.addInput 1.5 dag
+            let dag, cell4 =
+                Dag.buildFunction dag
+                    (fun s (n:int) (f:float) -> s + string n + "-" + string f)
+                |> Dag.applyCell cell1
+                |> Dag.applyCell cell2
+                |> Dag.applyCell cell3
+                |> Dag.addFunction
+            let! result = Dag.getValueTask cell4 dag |> Async.AwaitTask
+            Expect.equal "f8-1.5" result "f8-1.5"
+            let dag = Dag.setInput cell1 "w" dag
+            let! result = Dag.getValueTask cell4 dag |> Async.AwaitTask
+            Expect.equal "w8-1.5" result "w8-1.5"
+        }
+        testAsync "chained functions multi" {
+            let dag, cell1 = Dag.addInput "a" Dag.empty
+            let dag, cell2 = Dag.addInput 1 dag
+            let dag, cell3 =
+                Dag.buildFunction dag (fun s (n:int) -> "x:" + s + string n)
+                |> Dag.applyCell cell1
+                |> Dag.applyCell cell2
+                |> Dag.addFunction
+            let dag, cell4 = Dag.addInput "b" dag
+            let dag, cell5 = Dag.addInput 2 dag
+            let dag, cell6 =
+                Dag.buildFunction dag (fun s (n:int) -> "y:" + s + string n)
+                |> Dag.applyCell cell4
+                |> Dag.applyCell cell5
+                |> Dag.addFunction
+            let dag, cell7 = Dag.addInput "c" dag
+            let dag, cell8 = Dag.addInput 3 dag
+            let dag, cell9 =
+                Dag.buildFunction dag (fun s (n:int) -> "z:" + s + string n)
+                |> Dag.applyCell cell7
+                |> Dag.applyCell cell8
+                |> Dag.addFunction
+            let dagBefore, cell10 =
+                Dag.buildFunction dag
+                    (fun s1 s2 s3 -> String.concat "|" [s1;s2;s3])
+                |> Dag.applyCell cell3
+                |> Dag.applyCell cell6
+                |> Dag.applyCell cell9
+                |> Dag.addFunction
+            let dagAfter = Dag.setInput cell5 4 dagBefore
+            let! result = Dag.getValueTask cell10 dagAfter |> Async.AwaitTask
+            Expect.equal "x:a1|y:b4|z:c3" result "x:a1|y:b4|z:c3"
+            let! result = Dag.getValueTask cell10 dagBefore |> Async.AwaitTask
+            Expect.equal "x:a1|y:b2|z:c3" result "x:a1|y:b2|z:c3"
         }
     ]
 
 [<EntryPoint>]
-let main args =
-    runTestsWithArgs defaultConfig args tests
+let main args = runTestsWithArgs defaultConfig args tests
